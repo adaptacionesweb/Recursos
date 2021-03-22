4df9383f99ff28cf996804e03fa521bdddea0b79
adaptacionesweb <voer1976@hotmail.com>
2021-03-21 22:08:09 -0500

    Add files via upload

---
+++ firebase.init.js
@@ -0,0 +1,12 @@
+﻿// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA07Z5Zc7MWgwlP6cLvI2mCeuCbmRwVXGE",
+  authDomain: "usuarios-blogger.firebaseapp.com",
+  databaseURL: "https://usuarios-blogger-default-rtdb.firebaseio.com",
+  projectId: "usuarios-blogger",
+  storageBucket: "usuarios-blogger.appspot.com",
+  messagingSenderId: "955269743444",
+  appId: "1:955269743444:web:ce0b37ac96442abfddfec1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
